a3f6297e4091e80a2255b33669d688cc7749412d
wiljanverhoeven <wiljanverhoeven@xs4all.nl>
2024-10-22 15:01:28 +0200

    started contact page front end

---
+++ views/contact.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+    <head>
+        <link rel="stylesheet" href="../css/common.css" >
+        <link rel="stylesheet" href="../css/contact.css" >
+        
+    </head>
+    <body>
+        
+        <header >
+           <a class="home" href="../">HOME</a>
+
+        </header>
+        <script type="text/javascript" src="../js/stars.js"></script>
+        <main>
+        
+            <div class="container">
+            
+                <div class="left">
+                  
+                    
+                </div>
+                <div class="right">
+                    
+                </div>
+                    
+                
+            </div>
+
+        </main>
+
+        <footer>
+
+        </footer>
+
+    </body>
+</html>
