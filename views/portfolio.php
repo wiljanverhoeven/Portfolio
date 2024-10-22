c1039110496c9bff13910fbe32d4f7b6650ed30b
wiljanverhoeven <wiljanverhoeven@xs4all.nl>
2024-10-22 14:51:44 +0200

    made over mij page front end

--- views/portfolio.php
+++ views/portfolio.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
     <head>
+        <link rel="stylesheet" href="../css/common.css" >
         <link rel="stylesheet" href="../css/portfolio.css" >
+        
         
     </head>
     <body>
