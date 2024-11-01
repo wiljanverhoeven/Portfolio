863e74aae79bbe6ceb2fb4d230b8ed2568138577
wiljanverhoeven <wiljanverhoeven@xs4all.nl>
2024-11-01 14:10:26 +0100

    made filter

---
+++ views/project.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+    <head>
+       
+        <link rel="stylesheet" href="../css/common.css" >
+        <link rel="stylesheet" href="../css/project.css" >
+        <?php include '../dbconnect.php'; ?>
+        
+    </head>
+    <body>
+        
+        <header >
+        </header>
+        <script type="text/javascript" src="../js/stars.js"></script>
+        <main>
+            
+            
+            
+
+        </main>
+
+        <footer>
+
+        </footer>
+
+    </body>
+</html>
